7f8eb287cb5c15e2c9a730894ae1a76ebe8e07b0
shubhadeep13 <shubhadeepdasgupta2013@gmail.com>
2021-03-02 13:58:48 +0530

    new commit

--- src/test/java/stringcalc/AppTest.java
+++ src/test/java/stringcalc/AppTest.java
@@ -80,4 +80,13 @@ public class AppTest
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testStringCalcMod(){
+        try{
+            assertEquals(246, App.add("[243, 243]"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
